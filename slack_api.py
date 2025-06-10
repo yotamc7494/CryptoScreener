09b3942ed6bf8e6251c654376a9714f922d5ff21
yotam <yotamcohen1234@gmail.com>
2025-06-10 03:56:23 +0300

    fixed slack

--- slack_api.py
+++ slack_api.py
@@ -1,15 +1,22 @@
 import requests
 from config import SLACK_WEBHOOK_URL, SEND_SLACK
 
-
 def send_slack_alert(message):
     if not SEND_SLACK:
         return
-    payload = {"text": message}
-    print(f"Slack msg: {message}")
+    headers = {
+        "Authorization": f"Bearer {SLACK_WEBHOOK_URL}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "channel": "#general",
+        "text": message
+    }
     try:
-        response = requests.post(SLACK_WEBHOOK_URL, json=payload)
+        response = requests.post("https://slack.com/api/chat.postMessage", headers=headers, json=data)
         if response.status_code != 200:
             print(f"Slack error: {response.status_code} {response.text}")
+        else:
+            print(f"Send Slack Msg: {message}")
     except Exception as e:
         print(f"Failed to send Slack message: {e}")
